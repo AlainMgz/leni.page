fd07412bdab6552f7b5de13da5c35693d3a96bc9
AlainMgz <52154524+AlainMgz@users.noreply.github.com>
2026-06-15 00:00:03 +0200

    fix attempt #3

--- scripts/update-csp-headers.js
+++ scripts/update-csp-headers.js
@@ -26,12 +26,12 @@ for (const file of getAllHtmlFiles('./dist')) {
 
 const csp = [
   `default-src 'self'`,
-  `script-src 'self' https://static.cloudflareinsights.com ${[...hashes].join(' ')}`,
-  `style-src 'self' 'unsafe-inline'`,
+  `script-src 'self' ${[...hashes].join(' ')}`,
+  `style-src 'self'`,
   `style-src-attr 'none'`,
   `img-src 'self' data:`,
   `font-src 'self'`,
-  `connect-src 'self' https://static.cloudflareinsights.com`,
+  `connect-src 'self'`,
   `frame-ancestors 'none'`,
   `base-uri 'self'`,
   `form-action 'none'`,
